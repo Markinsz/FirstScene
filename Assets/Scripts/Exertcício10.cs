38a75a62bbffe6964962737d5fa5b4a0799f9a43
Markinsz <43195320827@info.local>
2024-11-07 15:22:14 -0300

    feat: exercícios feitos

--- Assets/Scripts/Exertcício10.cs
+++ Assets/Scripts/Exertcício10.cs
@@ -15,32 +15,45 @@ public class Exertcício10 : MonoBehaviour
 {
 
     [SerializeField] string Itens;
+    [SerializeField] int moeda = 10;
+    [SerializeField] int vida = 50;
+    [SerializeField] int mana = 25;
+    [SerializeField] int velocidade = 20;
+    [SerializeField] int defesa = 30;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        
-
         switch (Itens) { 
             case "moeda":
                 print("Tio Patinhas tá preocurando isso!");
+                moeda++;
                 break;
+
             case "poção de vida":
                 print("Full Health!");
+                vida = 100;
                 break;
+
             case "poção de mana":
                 print("Mana regen 100!");
+                mana = 100;
                 break;
+
             case "velocidade":
                 print("Rápido como um flash!");
+                velocidade = 50;
                 break;
+
             case "proteção":
                 print("Duro como pedra!");
+                defesa = 45;
                 break;
+
             case "flor de fogo":
                 print("Olha o copyright...");
                 break;
+
             default:
                 Debug.Log("Escolha entre moeda, poção de vida, poção de mana, velocidade, proteção ou flor de fogo");
                 break;
